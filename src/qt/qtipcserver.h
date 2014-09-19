e635dbaf43442a3452c88fdd4fec546d28d8da7f
roscoin1 <godivacoin@yahoo.com>
2014-09-19 17:56:03 -0400

    sweet currency

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define Roscoin-Qt message queue name
+#define ROSCOINURI_QUEUE_NAME "RoscoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
